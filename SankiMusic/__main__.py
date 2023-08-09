792d6b573a3c26e6b79a6ee92bb1bf91d7b03163
HELLSPAMXD <134424056+HELLSPAMXD@users.noreply.github.com>
2023-08-09 10:22:00 +0530

    Update __main__.py

--- SankiMusic/__main__.py
+++ SankiMusic/__main__.py
@@ -64,7 +64,7 @@ async def init():
     except:
         pass
     await Kaal.decorators()
-    LOGGER("SankiMusic").info("Congratulations, Your isha Bot Now Deployed ...")
+    LOGGER("SankiMusic").info("Congratulations, Your  Lucia-xbot Now Deployed ...")
     await idle()
 
 
